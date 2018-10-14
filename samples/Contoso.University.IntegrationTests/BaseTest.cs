fd65c570cc146c12fb1b5d1f7a6c943cea66396b
Tiago Soczek <tiagosoczek@gmail.com>
2018-10-14 13:42:44 -0300

    Organize BaseTest

--- samples/Contoso.University.IntegrationTests/BaseTest.cs
+++ samples/Contoso.University.IntegrationTests/BaseTest.cs
@@ -1,52 +1,25 @@
 ﻿using MediatR;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.TestHost;
-using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.IO;
 using System.Net.Http;
 using Xunit;
 
 namespace Contoso.University.IntegrationTests
 {
-    public class IntegrationTestFixture : IDisposable
-    {
-        public IntegrationTestFixture()
-        {
-            // More info about MVC Testing https://blogs.msdn.microsoft.com/webdev/2017/12/07/testing-asp-net-core-mvc-web-apps-in-memory/
-
-            var contentRoot = @"..\..\..\..\Contoso.University";
-
-            var hostBuilder = WebHost.CreateDefaultBuilder()
-                .UseContentRoot(Path.GetFullPath(contentRoot))
-                .UseEnvironment("Test")
-                .UseStartup<Startup>();
-
-            TestServer = new TestServer(hostBuilder);
-        }
-
-        public TestServer TestServer { get; }
-
-        public void Dispose()
-        {
-            TestServer?.Dispose();
-        }
-    }
-
     public abstract class BaseTest : IClassFixture<IntegrationTestFixture>
     {
+        private readonly IntegrationTestFixture fixture;
+
         protected BaseTest(IntegrationTestFixture fixture)
         {
-            Host = fixture.TestServer.Host;
-            Client = fixture.TestServer.CreateClient();
-            Mediator = GetService<IMediator>();
+            this.fixture = fixture;
         }
 
-        protected HttpClient Client { get; }
-        protected IWebHost Host { get; }
-        protected IMediator Mediator { get; }
+        protected HttpClient Client => fixture.Client;
 
-        protected TService GetService<TService>() => Host.Services.GetRequiredService<TService>();
+        protected IWebHost Host => fixture.Host;
+
+        protected IMediator Mediator => fixture.Mediator;
+
+        protected TService GetService<TService>() => fixture.GetService<TService>();
     }
 }
---
+++ samples/Contoso.University.IntegrationTests/IntegrationTestFixture.cs
@@ -0,0 +1,48 @@
+﻿using MediatR;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
+using System.Net.Http;
+
+namespace Contoso.University.IntegrationTests
+{
+    public class IntegrationTestFixture : IDisposable
+    {
+        public IntegrationTestFixture()
+        {
+            // More info about MVC Testing https://blogs.msdn.microsoft.com/webdev/2017/12/07/testing-asp-net-core-mvc-web-apps-in-memory/
+
+            var contentRoot = @"..\..\..\..\Contoso.University";
+
+            var hostBuilder = WebHost.CreateDefaultBuilder()
+                .UseContentRoot(Path.GetFullPath(contentRoot))
+                .UseEnvironment("Test")
+                .UseStartup<Startup>();
+
+            TestServer = new TestServer(hostBuilder);
+
+            Host = TestServer.Host;
+            Client = TestServer.CreateClient();
+            Mediator = GetService<IMediator>();
+        }
+
+        public TestServer TestServer { get; }
+
+        public HttpClient Client { get; }
+
+        public IWebHost Host { get; }
+
+        public IMediator Mediator { get; }
+
+        public TService GetService<TService>() => Host.Services.GetRequiredService<TService>();
+
+        public void Dispose()
+        {
+            Client?.Dispose();
+            TestServer?.Dispose();
+        }
+    }
+}
