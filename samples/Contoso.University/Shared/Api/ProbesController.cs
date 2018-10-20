51112d97a3327630c85f94fffb7f52e2499bdf8e
Tiago Soczek <tiagosoczek@gmail.com>
2018-10-20 16:18:29 -0300

    Dist folder ignored; Healthz added;

---
+++ samples/Contoso.University/Shared/Api/ProbesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Contoso.University.Shared.Api
+{
+    [Route("_")]
+    public class ProbesController : Controller
+    {
+        [HttpGet("healthz")]
+        public IActionResult Healthz()
+        {
+            return Ok();
+        }
+    }
+}
